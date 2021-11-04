69c4d7a7fe85e305423d4353520008da08d1b4fc
ReShadowww <justas.adomas.l@gmail.com>
2021-11-04 17:44:06 +0200

    up

--- init/init.sql
+++ init/init.sql
@@ -1,16 +1,33 @@
-DROP DATABASE if EXISTS smdcal;
-CREATE DATABASE smdcal;
+-- --------------------------------------------------------
+-- Host:                         185.80.130.117
+-- Server version:               8.0.27 - MySQL Community Server - GPL
+-- Server OS:                    Linux
+-- HeidiSQL Version:             11.3.0.6295
+-- --------------------------------------------------------
 
-ALTER DATABASE smdcal CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-USE smdcal;
 
-create table bureliai(
-    id int auto_increment primary key,
-    burelis varchar(255) not null,
-    UNIQUE (burelis)
-);
+-- Dumping database structure for smd_cal
+CREATE DATABASE IF NOT EXISTS `smdcal` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `smdcal`;
 
+-- Dumping structure for table smd_cal.bureliai
+CREATE TABLE IF NOT EXISTS `bureliai` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `burelis` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `burelis` (`burelis`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table smd_cal.bureliai: ~23 rows (approximately)
+DELETE FROM `bureliai`;
+/*!40000 ALTER TABLE `bureliai` DISABLE KEYS */;
 INSERT INTO `bureliai` (`id`, `burelis`) VALUES
 	(10, 'Abdominalinės chirurgijos būrelis'),
 	(16, 'Akių ligų būrelis'),
@@ -35,3 +52,9 @@ INSERT INTO `bureliai` (`id`, `burelis`) VALUES
 	(20, 'Urologijos būrelis'),
 	(21, 'Veido ir žandikaulių chirurgijos būrelis'),
 	(22, 'Veterinarinės medicinos būrelis');
+/*!40000 ALTER TABLE `bureliai` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
